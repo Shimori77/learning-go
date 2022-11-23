3dfc3d88fc76230610e8aebdd0e5a09218a67452
shimori77 <shimori@tuta.io>
2022-11-23 16:39:32 -0300

    Arrays and Slices

--- main/arrays.go
+++ main/arrays.go
@@ -1,1 +1,17 @@
 package main
+
+import "fmt"
+
+func printArrays() {
+	fmt.Println("--- Arrays and Slices ---")
+	var arr [3]int
+	arr[0] = 3
+	arr[1] = 2
+	arr[2] = 1
+
+	fmt.Println(arr)
+
+	arr0 := [3]int{4, 5, 6}
+	fmt.Println(arr0)
+
+}
